f8a8d2e29eabe4b219cf9105cc7828d70afdcbfb
kaonthu <103020581+kaonthu@users.noreply.github.com>
2023-01-23 17:12:32 +0800

    Add files via upload

---
+++ 4. 檢體相關/4.1 檢體資料庫/4.1.1 編碼資訊/111編碼對照表/plasma_2022.R
@@ -0,0 +1,88 @@
+
+f=function(date,person,speciem,Pstart,P_number_start,Cstart,C_number_start,Box_start,Site_start)
+
+{
+
+S_number_a <- rep(date,person*5)
+S_number_b <- rep(sprintf('%0.3d', 1:person),each = 5)
+S_number <- paste(S_number_a,S_number_b,sep="")
+
+P_number_a <- rep(Pstart,person*5)
+P_number_b <- rep(sprintf('%0.4d', (P_number_start):(P_number_start+person-1)),each = 5)
+P_number  <-  paste(P_number_a,P_number_b,sep="")
+
+C_number_a <- rep(Cstart,person*5)
+C_number_b <- sprintf('%0.5d', (C_number_start):(C_number_start+person*5-1))
+C_number  <-  paste(C_number_a,C_number_b,sep="")
+
+
+Site <- ((Site_start-1  + 0:(person*5-1)) %% 100) + 1
+
+Box <- Box_start  + (Site_start + (-1):(person*5-2)) %/% 100
+
+Box <- sprintf('%0.4d',Box)
+
+Box <- paste('P',Box,sep="")
+
+OUT <- data.frame(S_number_a,S_number,P_number,substr(P_number,3,7),C_number,Box,Site)
+
+print(data.frame(QC_miss_person,QC_miss_number))
+
+
+QC_start <- rep(5,person)
+
+if( length(QC_miss_person) > 0)
+
+{
+
+delete <- NULL
+
+for (i in 1:length(QC_miss_person)) {
+QC_start[QC_miss_person[i]] <- QC_start[QC_miss_person[i]]-QC_miss_number[i]
+delete <- c(delete,(5*QC_miss_person[i]-QC_miss_number[i]+1) : (5*QC_miss_person[i]))
+}
+
+OUT2 <- OUT[-delete,]
+
+QC <- rep(0,person*5 - length(delete))
+QC[cumsum(QC_start)] <- 1
+
+
+QC_start <- rep(5,person)
+
+delete <- NULL
+
+for (i in 1:length(QC_miss_person)) {
+QC_start[QC_miss_person[i]] <- QC_start[QC_miss_person[i]]-QC_miss_number[i]
+delete <- c(delete,(5*QC_miss_person[i]-QC_miss_number[i]+1) : (5*QC_miss_person[i]))
+}
+
+OUT2 <- OUT[-delete,]
+
+QC <- rep(0,person*5 - length(delete))
+QC[cumsum(QC_start)] <- 1
+
+OUT3 <- data.frame(OUT2,"P",QC)
+
+}else {  
+
+ QC <- rep(c("","","","","QC"),person)
+ OUT3 <- data.frame(OUT,"P",QC)    }
+
+print(OUT3[(dim(OUT3)[1]-5):dim(OUT3)[1],])
+return(OUT3)
+
+}
+
+#################################################################
+
+QC_miss_person <- NULL
+QC_miss_number <- NULL
+
+
+Plasma_2022 <- f(20220305,1000,"P","P11",1,"C06",1,1,1)
+#此場次最後一人少了一管，下一管C number 要多加1 回去
+
+write.table(Plasma_2022,"D:/基隆長庚社區醫學科研究中心/CMRC社區資料庫/20220112檢體登記/111編碼對照表(進行中)/plasma_2022.csv",sep=",")
+
+#################################################################
